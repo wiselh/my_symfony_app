0e1ab6c72b13cb94c9a233e2a87db077ba9868a6
hakimlh <abdelhakim.lahlou@gmail.com>
2018-02-11 18:03:39 +0000

    some routing update

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -3,11 +3,16 @@
 namespace ProductBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/test",name="homepage")
+     */
     public function indexAction()
     {
-        return $this->render('ProductBundle:Default:index.html.twig');
+//        die("It works !!");
+        return $this->render('@ProductBundle:Default:index.html.twig');
     }
 }
