9f7821bc464bd499481bd690ca8d64c8db90c43b
hakimlh <abdelhakim.lahlou@gmail.com>
2018-02-15 11:52:53 +0000

    For item in blogs if item.id

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -2,12 +2,37 @@
 
 namespace BlogBundle\Controller;
 
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BlogBundle:Default:index.html.twig');
+    /**
+     * @Route("/blog",name="blog_page")
+     */
+    public function indexAction(){
+
+        $blogs =[
+            [
+                'id' => 1,
+                'title' => 'article 1',
+                'content' => 'lorem lorem 1'
+            ],
+            [
+                'id' => 2,
+                'title' => 'article 2',
+                'content' => 'lorem lorem 2'
+             ],
+             [
+                'id' => 3,
+                'title' => 'article 3',
+                'content' => 'lorem lorem 3'
+            ]
+        ];
+
+        return $this->render('@Blog/Default/index.html.twig',['blogs' => $blogs]);
     }
 }
