00837648a786212c68023730afd09b0a07731455
hakimlh <abdelhakim.lahlou@gmail.com>
2018-02-17 20:24:02 +0000

    Generate a Form Type "ProductType"
    Run : php bin/console generate:doctrine:form ProductBundle:Product

---
+++ src/ProductBundle/Form/ProductType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('description')->add('price')->add('createdAt')->add('category')->add('picture');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProductBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'productbundle_product';
+    }
+
+
+}
